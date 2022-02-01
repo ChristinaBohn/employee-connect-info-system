9d78645fa1cf2572fd13c3e6e194c2643c3b50f8
Christina Bohn <christinajbohn@gmail.com>
2022-02-01 15:51:34 -0800

    Installed proper dependencies, begain pseudocode for application.

---
+++ index.js
@@ -0,0 +1,28 @@
+// Present user with options
+
+
+// View all departments - READ - "SELECT * FROM table_name/departments";
+
+// View all roles - READ - "SELECT * FROM roles";
+
+// View all employees - READ - "SELECT * FROM employees";
+   
+    // Can use JOIN to add more info
+
+
+// Add a department - CREATE - "INSERT INTO table_name (col1, col2) VALUES (val1, val2)"
+
+// Add a role - CREATE - "INSERT INTO table_name (col1, col2) VALUES (val1, val2)"
+    
+    // SELECT the existing roles from the 'roles' table
+
+    // .map() the results from 'roles' to question data for inquirer (going to need the id)
+    
+    // THEN prompt the user for role info (inquirer)
+
+        // Store user's answers and INSERT them into the 'roles' table
+
+// Add an employee - CREATE - "INSERT INTO table_name (col1, col2) VALUES (val1, val2)"
+
+
+// Update an employee
