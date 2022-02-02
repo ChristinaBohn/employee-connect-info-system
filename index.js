0fa654a3ce0670d81d41d6ee029582757b4ecdf9
Christina Bohn <christinajbohn@gmail.com>
2022-02-01 16:08:12 -0800

    Added questions for user prompts.

--- index.js
+++ index.js
@@ -1,5 +1,63 @@
 // Present user with options
 
+const nextActionQuestion = [
+    {
+        type: 'list',
+        name: 'nextQuestion',
+        message: 'What would you like to do?',
+        choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add Department', 'Add Role', 'Add Employee', 'Update Employee Role']
+    }
+];
+
+const addDepartmentQuestions = [
+    {
+        type: 'input',
+        name: 'departmentName',
+        message: 'What is the name of the new Department?'
+    }
+];
+
+const addRoleQuestions = [
+    {
+        type: 'input',
+        name: 'roleName',
+        message: 'What is the name of the new Role?'
+    },
+    {
+        type: 'input',
+        name: 'roleSalary',
+        message: 'What is the salary for this Role'
+    },
+    {
+        // Drop down choices for this question?
+        type: 'input',
+        name: 'roleDepartment',
+        message: 'Which Department is this Role in?'
+    }
+];
+
+const addEmployeeQuestions = [
+    {
+        type: 'input',
+        name: 'employeeFirstName',
+        message: 'What is the first name of the new Employee?'
+    },
+    {
+        type: 'input',
+        name: 'employeeLastName',
+        message: 'What is the last name of the new Employee?'
+    },
+    {
+        type: 'input',
+        name: 'employeeRole',
+        message: 'What is the new Employee\'s Role?'
+    },
+    {
+        type: 'input',
+        name: 'employeeManager',
+        message: 'Who is the new Employee\'s manager?'
+    }
+];
 
 // View all departments - READ - "SELECT * FROM table_name/departments";
 
